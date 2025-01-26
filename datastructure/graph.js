3a4d55518e603a8f93fd6af0174e83721de59c05
Aditya <parmaraditya242@gmail.com>
2025-01-27 01:31:15 +0530

    added dfs method in graph

--- datastructure/graph.js
+++ datastructure/graph.js
@@ -6,22 +6,32 @@
       this.#adjmatrix=new Array(nodes)
       for(let i=0;i<this.#adjmatrix.length;i++){
         this.#adjmatrix[i]=[]
-        for(let j=0;j<nodes;j++){
-            this.#adjmatrix[i][j]=0
-        }
       }
       }
       addedge(u,v){
-  this.#adjmatrix[u][v]=1
-  this.#adjmatrix[v][u]=1
+  this.#adjmatrix[u].push(v)
+  this.#adjmatrix[v].push(u)
     } 
+
+    dfs(start){
+      let stack=[start]
+      let visited=new Set()
+      while(stack.length>0){
+        let node=stack.pop()
+        if(!visited.has(node)){
+          console.log(node)
+          visited.add(node)
+        }
+        for(let i=this.#adjmatrix[node].length-1;i>=0;i--){
+          if(!visited.has(this.#adjmatrix[node][i])){
+            stack.push(this.#adjmatrix[node][i])
+          }
+        }
+      }
+    }
       print(){
-        for(let i of this.#adjmatrix){
-            let s=""
-           for(let j of i){
-           s+=j+" "
-           }
-           console.log(s)
+        for(let i=0;i<this.#adjmatrix.length;i++){
+           console.log(`${i}->${this.#adjmatrix[i]}`)
         }
       }
    }
@@ -44,7 +54,6 @@ addedge(u,v){
   this.#adjlist[u].append(v)
   this.#adjlist[v].append(u)
 }
-
 print(){
   for(let i=0;i<this.#adjlist.length;i++){
     let curr=this.#adjlist[i].head
@@ -56,10 +65,40 @@ print(){
     console.log(s)
   }
   }
-  
+   
+dfs(start){
+  let stack=[start]
+  let visited=new Set()
+  while(stack.length>0){
+    let node=stack.pop()
+    if(!visited.has(node)){
+      console.log(node)
+      visited.add(node)
+    }
+    
+    let curr=this.#adjlist[node].head
+    
+    while(curr){
+      if(!visited.has(curr.value)){
+        stack.push(curr.value)
+      }
+      curr=curr.next
+    }
+  }
+}
 
 
-   }
+}
+
+
+
+   let gr=new linkedgraph(4)
+   gr.addedge(0,1)
+   gr.addedge(1,2)
+   gr.addedge(1,3)
+   gr.addedge(2,0)
+   gr.dfs(0)
+   gr.print()
 
 
    
