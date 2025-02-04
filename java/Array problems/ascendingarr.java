29023cde0824ed060d3a16d2f95d170127c55207
Aditya <parmaraditya242@gmail.com>
2025-02-04 22:52:12 +0530

    maximum sum of ascending arr

---
+++ java/Array problems/ascendingarr.java
@@ -0,0 +1,20 @@
+ class ascendingarr {
+
+   int maximumsum(int[] nums){
+    int maximumsum=nums[0];
+    int sum=nums[0];
+    for(int i=1;i<nums.length;i++){
+       if(nums[i]>nums[i-1]){
+        sum+=nums[i];
+       }else{
+        sum=nums[i];
+       }
+       maximumsum=Math.max(maximumsum,sum);
+    }
+    return maximumsum;
+}
+   public static void main(String[] args) {
+    
+   }
+    
+}
